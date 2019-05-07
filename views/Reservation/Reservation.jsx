585d3dcbd0de435344f3f6d1d51c03610531d000
dogrodnik <d.ogrodnik96@gmail.com>
2019-05-07 20:47:09 +0200

    Reservation - code update

--- views/Reservation/Reservation.jsx
+++ views/Reservation/Reservation.jsx
@@ -1,112 +1,15 @@
 import React from 'react';
-import { ReservationDiv, AddIcon, AddButton } from './ReservationStyles';
+import { ReservationDiv } from './ReservationStyles';
+import Tickets from './Tickets.jsx';
+import Screen from './Screen.jsx';
 
-// Przenieś wszystko do osobnych komponnetów, to jest widok gdzie będą tylko wywoływane komponenty
-class Reservation extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      normal: 0,
-      reduced: 0,
-      senior: 0,
-      total: 0,
-      price: 0,
-    };
-  }
-
-  // Zamiast klas uzywania klas rób styled-components, po to one są
-  // render() musi być ostatnią metodą w klasie
-  render() {
-    return (
-      <ReservationDiv>
-        <div className="reservation-container">
-          <div className="border reservation"> Rezerwacja </div>
-          <div className="border tickets">
-            <div className="flex">Wybierz bilety </div>
-            <TicketType
-              text="Normalny"
-              title="normal"
-              ticket={this.state.normal}
-              price="20"
-              click={this.IncreaseDecrease}
-            />
-            <TicketType
-              text="Ulgowy"
-              title="reduced"
-              ticket={this.state.reduced}
-              price="15"
-              click={this.IncreaseDecrease}
-            />
-            <TicketType
-              text="Senior"
-              title="senior"
-              ticket={this.state.senior}
-              price="10"
-              click={this.IncreaseDecrease}
-            />
-            <div className="flex row">
-              <div className="summary">Ilość biletów</div> <div className="summary">{this.state.total} </div>{' '}
-            </div>
-            <div className="flex row">
-              <div className="summary">Cena</div> <div className="summary"> {this.state.price} zł </div>{' '}
-            </div>
-            <AddButton className="flex" type="primary">
-              {' '}
-              Rezerwuj{' '}
-            </AddButton>
-          </div>
-          <div className="flex border screen-view"> Screen </div>
-        </div>
-      </ReservationDiv>
-    );
-  }
-
-  // a propos tego co podkreśla eslint
-  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-access-state-in-setstate.md
-  IncreaseDecrease = event => {
-    const name = event.currentTarget.parentElement.className.split(' ');
-    const newState = {};
-    if (name[0] === 'plus') {
-      newState[name[1]] = parseInt(event.currentTarget.parentElement.getAttribute('value'), 10) + 1;
-      this.setState(newState, function update() {
-        // używaj ES6, czyli funkcji strzałkowych
-        this.setState({ total: this.state.normal + this.state.reduced + this.state.senior }, function update2() {
-          this.setState({ price: this.state.normal * 20 + this.state.reduced * 15 + this.state.senior * 10 });
-        });
-      });
-    } else {
-      if (event.currentTarget.parentElement.getAttribute('value') == 0) return;
-      newState[name[1]] = parseInt(event.currentTarget.parentElement.getAttribute('value'), 10) - 1;
-      this.setState(newState, function update() {
-        this.setState({ total: this.state.normal + this.state.reduced + this.state.senior }, function update2() {
-          this.setState({ price: this.state.normal * 20 + this.state.reduced * 15 + this.state.senior * 10 });
-        });
-      });
-    }
-  };
-}
-
-// !!!! KAŻDY KOMPONENET TO OSOBNY PLIK !!!!
-const TicketType = props => {
+const Reservation = () => {
   return (
-    <div className="flex row">
-      <div className="counter"> {props.text} </div>
-      <div className="price counter">
-        <div className={`minus ${props.title}`} value={props.ticket}>
-          <AddIcon type="minus-circle" onClick={props.click} />
-        </div>
-        <div> {props.ticket} </div>
-        <div className={`plus ${props.title}`} value={props.ticket}>
-          <AddIcon type="plus-circle" onClick={props.click} />
-        </div>
-      </div>
-      <div className="price counter"> {props.ticket * props.price} zł </div>
-    </div>
+    <ReservationDiv>
+      <Tickets />
+      <Screen />
+    </ReservationDiv>
   );
 };
-
-// PROPS VALIDATION
-
-//
 
 export default Reservation;
--- views/Reservation/ReservationStyles.js
+++ views/Reservation/ReservationStyles.js
@@ -1,56 +1,8 @@
 import styled from 'styled-components';
-import { Icon, Button } from 'antd';
 
 export const ReservationDiv = styled.div`
-  .reservation-container {
-    font-size: 36px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-  }
-  .reservation {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-  }
-  .tickets {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-content: stretch;
-    align-items: stretch;
-  }
-  .screen-view {
-    min-width: 800px;
-  }
-  .flex {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  .flex.row {
-    justify-content: space-between;
-    margin: 20px;
-  }
-  .counter {
-    display: flex;
-    width: 20vh;
-  }
-  .border {
-    border: 3px black solid;
-  }
-  .price {
-    display: flex;
-    justify-content: flex-end;
-  }
+  font-size: 36px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 `;
-
-export const AddIcon = styled(Icon)`
-  font-size: 30px;
-  margin: 10px;
-`;
-export const AddButton = styled(Button)`
-  margin: 10px;
-  font-size: 12px;
-`;
-// export default reservationDiv;
