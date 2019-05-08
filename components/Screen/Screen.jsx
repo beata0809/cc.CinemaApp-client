7249c5322ea4436317d1a92f65fa0b0870c2d188
Risusagi <anastazja199707@gmail.com>
2019-05-08 21:21:40 +0200

    Sala kinowa start

--- components/Screen/Screen.jsx
+++ components/Screen/Screen.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
-import { ScreenDiv } from './ScreenStyles';
+import { ScreenDiv, OneSit, SitsDiv, ScreenInHall, LeftRows, RightRows } from './ScreenStyles';
 
 const Screen = () => {
-  return <ScreenDiv>Screen</ScreenDiv>;
+  const renderSits = () => {
+    const sits = [];
+    for (let i = 0; i < 60; i++) {
+      sits.push(`${i}`);
+    }
+    return sits.map(sit => (
+      <OneSit></OneSit>
+    ));
+  }
+  const renderRows = () => {
+    const rows = [];
+    for (let i = 0; i < 6; i++) {
+      rows.push(String.fromCharCode(65 + i));
+    }
+    return rows.map(row => (
+      <p>{row}</p>
+    ));
+  }
+  renderRows();
+  return (
+    <ScreenDiv>
+      <ScreenInHall>
+        <p>Ekran</p>
+      </ScreenInHall>
+      <LeftRows>{renderRows()}</LeftRows>
+      <SitsDiv> {renderSits()}</SitsDiv>
+      <RightRows>{renderRows()}</RightRows>
+    </ScreenDiv>
+  );
 };
 
 export default Screen;
--- components/Screen/ScreenStyles.js
+++ components/Screen/ScreenStyles.js
@@ -3,8 +3,34 @@ import styled from 'styled-components';
 export const ScreenDiv = styled.div`
   min-width: 800px;
   min-height: 500px;
+  ${'' /* width: 800px; */}
+  ${'' /* height: 500px; */}
   border: 3px black solid;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  ${'' /* display: flex; */}
+  ${'' /* justify-content: center; */}
+  ${'' /* align-items: center; */}
+  display: grid;
+  grid-template-areas: "screen screen screen"
+                       "rows-left sits rows-right"
+`;
+
+export const OneSit = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: lightgrey;
+`;
+export const SitsDiv = styled.div`
+  grid-area: sits;
+  display: grid;
+  grid-template-columns: repeat(10, 1fr);
+  grid-row-gap: 10px;
+`;
+export const ScreenInHall = styled.div`
+  grid-area: screen;
+`;
+export const LeftRows = styled.div`
+  grid-area: rows-left;
+`;
+export const RightRows = styled.div`
+  grid-area: rows-right;
 `;
