5c8ff63b49cb5962bc2d7e669e298da3ec153839
Jan Poniatowski <poniatowski126@gmail.com>
2019-05-14 20:49:26 +0200

    merge

--- components/DatePanel/DatePanel.jsx
+++ components/DatePanel/DatePanel.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { func } from 'prop-types';
 import { Link } from 'react-router-dom';
-import { DateDiv, DateContainer } from './DatePanelStyles.js';
 import { catchDate } from '../../store/actions';
 import { DateDiv, DateContainer, DateAnchor } from './DatePanelStyles.js';
 
-const DatePanel = () => {
-  const changeTheFirst = (e) => {
+const DatePanel = props => {
+  const getDate = (e, date) => {
+    e.preventDefault();
+    props.catchDate(date);
+  };
+
+  const changeTheFirst = e => {
     const firstDiv = document.querySelector('a > *'); //1. div z datami
     if (e.target !== firstDiv && e.target !== firstDiv.childNodes[0] && e.target !== firstDiv.childNodes[1]) {
       firstDiv.classList.add('not-focused'); // jeżeli kliknieto nie 1. div, lub elementy wewnątrz niego to nadaj mu klasę not-focused
@@ -15,7 +19,7 @@ const DatePanel = () => {
     firstDiv.addEventListener('click', () => {
       firstDiv.classList.remove('not-focused'); //usun klasę not-focused w razie kliknięcia w 1. div z datą
     });
-  }
+  };
   const renderDates = () => {
     const dates = [];
     const weekDaysNames = ['Niedziela', 'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota'];
@@ -32,7 +36,7 @@ const DatePanel = () => {
     }
     return dates.map(date => (
       <DateAnchor href="#" key={`${date.day}${date.month}`} onClick={changeTheFirst}>
-        <DateContainer>
+        <DateContainer onClick={e => getDate(e, date)}>
           <h2>
             {date.day}.{date.month}
           </h2>
@@ -44,7 +48,6 @@ const DatePanel = () => {
 
   return <DateDiv>{renderDates()}</DateDiv>;
 };
-
 
 DatePanel.propTypes = {
   catchDate: func,
